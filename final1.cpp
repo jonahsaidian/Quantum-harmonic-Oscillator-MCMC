39ff974e8bbea7e9469eed34f82aab2afe3cb527
jonahsaidian <85853206+jonahsaidian@users.noreply.github.com>
2021-06-14 13:20:27 -0700

    Add files via upload

---
+++ final1.cpp
@@ -0,0 +1,131 @@
+/**************************************************/
+//    Atoms in a Magnetic Trap Simulation         //
+//    For Physics 142 at UC San Diego             //
+//    Code for Final Project                      //
+/**************************************************/
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cmath>
+#include<cstdlib>
+#include<numeric>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  const int N = 100;         //  Particle number
+  int pos_bin[1000]= {0};     //  for binning location hits
+  double dx = 0.01;           //  for good acceptance rate
+  double k = 0.5;
+  double T = .00010;      //  Includes Temp and Boltz Const
+  double tempX[3];            //  Holds Test step
+  srand((unsigned)time(NULL));
+  FILE *rhist;
+  rhist = fopen("./rhist.c","w");
+
+  double **X;
+  X = new double*[N];
+  for(int i =0; i < N; i++)
+    X[i] = new double[3];
+  for(int i =0; i < N; i++)   //  Initialize Gas
+    for(int j =0; j < 3; j++)
+      X[i][j] =0.04*dx*((double)rand() / RAND_MAX - 0.5);
+
+  long count = 0;
+  long n = 0;
+  while( n < 100000)
+    {
+      for (int i = 0; i < N; i++ )
+	{
+	  for(int j =0; j < 3; j++)   // step in 3D
+	    tempX[j] = X[i][j] + dx*(2.0*(double)rand() / RAND_MAX -1);
+	  double DE = k*(std::inner_product(tempX,tempX+3,tempX,0.0)
+			 -std::inner_product(X[i],X[i]+3,X[i],0.0));
+	  //  cout << DE << endl;   // for troubleshooting
+	  //  cout << exp(-DE/T) << endl;
+	  if ((double)rand()/RAND_MAX < exp(-DE/T))
+	    for(int j =0; j < 3; j++)
+	      X[i][j]=tempX[j];
+	  if(X[i][1]== tempX[1])
+	    count++;
+	}
+      n++;
+      //cout << static_cast<double>(count)/(n*N)<< endl;  // for troubleshooting
+    }
+  count = 0;                        // re-clock
+  long measure_count = 0;           // for average calculations
+  double E,E2,Eave,Eerror,E2ave;    // for E and E2 ave on path and E ave over MC Sim
+  double Etot = 0;                  // to hold E after every 100 steps
+  double E2tot = 0;                 // to hold E sqrd for every 100 steps
+  n =0;                             // reset n for convenience
+
+  std::ofstream outFile;
+  outFile.open("pos2.dat");
+  outFile << std::setprecision(6) << std::fixed << std::showpoint;   // outputing waveform
+
+  while( n < 1000000)
+    {
+      for (int i = 0; i < N; i++ )
+	{
+	  for(int j =0; j < 3; j++){
+	    tempX[j] = X[i][j] + dx*(2.0*(double)rand() / RAND_MAX - 1.0);}
+	  double DE = k*(std::inner_product(tempX,tempX+3,tempX,0.0)
+			 -std::inner_product(X[i],X[i]+3,X[i],0.0));
+	    //(std::inner_product(tempX, tempX+3, tempX, 0.0)
+	    //		 -std::inner_product(X[i], X[i]+3, X[i], 0.0));
+	  if ((double)rand()/RAND_MAX < exp(-DE/T))
+	    for(int j =0; j < 3; j++)
+	      X[i][j]=tempX[j];
+	  if(X[i][1]== tempX[1])
+	    count++;
+	}
+      if(0 == n %100)     // measure every 100
+	{
+	  measure_count++;
+	  double Etemp;
+	  E = 0;
+	  E2 = 0;
+
+	  for(int i = 0; i < N; i++)
+	    {
+	      double R = std::inner_product(X[i],X[i]+3,X[i],0.0);
+	      Etemp = k*R;
+	      R = sqrt(R);
+	      E +=Etemp;
+	      fprintf(rhist,"%f  %f  %f  \n",X[i][0],X[i][1],X[i][2]);
+	      //      E2 += Etemp*Etemp;                   // average over gas
+	      for(int j =0; j < 1000; j++)
+		if(R >=j*.000003 && R < (j+1)*.000003)
+		  pos_bin[j]++;
+	    }
+	  Etot += E;
+	  E2tot += E*E;
+	  Eave = Etot/(static_cast<double>(measure_count));               // Average over MC Sim
+	  E2ave = E2tot/(static_cast<double>(measure_count));
+	  Eerror = sqrt((E2ave - Eave*Eave)/(static_cast<double>(measure_count)));
+	  if(0 == n%10000){
+	    cout << "Percent done: " << n/10000.0 <<  "%" << endl;
+	    cout << "Average energy: "<< Eave/N << "\tError in E is:  " << Eerror << endl;
+	    cout << "Acceptance Rate:  " <<static_cast<double>(count)/(N*n) << endl;
+	  }}
+      n++;
+
+    }
+  for(int j =0; j < N; j++)
+    delete X[j];
+  delete X;
+  cout << "\n\n\n" << endl;
+  cout << "Analytical Solutions: " << 3.0*T/2.0 << endl;
+  cout << "Finalaverage E is : " << Etot/(static_cast<double>(measure_count)*N) << endl;
+  cout << "Final Error over Monte Carlo Simulation: " << Eerror << endl;
+  for(int i = 0; i < 1000; i++)
+    outFile <<  i*.000003 + 0.0000015 << "\t" << pos_bin[i] << endl;
+  outFile.close();
+  return 0;
+  }
---
+++ mcmc3d.c
@@ -0,0 +1,147 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define Maxpnt  5000 /* the maximum number of points. this can be changed for a larger system */
+#define dim 3
+#define G 6.67408e-11 /* units of m3 /kg /s2 */
+#define kpc 3.086e19 /*conversion of meters to kiloparsecs */
+#define Msolar 1.98855e30 /* mass of the sun in kg */
+#define Pi 3.14159265358979323846
+#define bmass 5.683e26 /* the mass of the central  body */
+#define lmass 1e24 /* the mass of the orbiting bodies*/
+/*#define baxis sqrt(1-(ecc*ecc))*axis */
+#define ecc sqrt(1-(baxis*baxis)/(axis*axis))
+
+void main(argc, argv)
+int argc;
+char *argv[];
+{
+    int i,sweep,burn,meas,ind,u=0,j,N;
+    double pos[Maxpnt],x[Maxpnt],y[Maxpnt],z[Maxpnt],dr, dx,dy,dz, pos1,x1,y1,z1,r,delta,ds,l=0,acc=0,NN,x2,z2,y2,pos2,e=0,e2=0;
+    srand((unsigned)time(NULL));
+
+    double h=1.0;
+    double m=1.0;
+    double w=1.0;
+    double tau=1000;
+    N = 2000;
+    NN= 2000.0;
+    double a=tau/NN;
+    delta=.6;
+    double c=pow(a*w,2.0);
+    double w2=pow(w,2.0);
+    double ee=0.0,ee2=0.0;
+
+    burn=100000;
+    meas=1000000;
+    ind=1000;
+
+    FILE *poshist;
+    FILE *pos2hist;
+    poshist = fopen("./poshist.c","w");
+    pos2hist = fopen("./pos2hist.c","w");
+
+
+    for(i=0;i<N;i++)
+    {
+        x[i]=0.0;
+        y[i]=0.0;
+        z[i]=0.0;
+    }
+    x[N]=x[0];
+    y[N]=y[0];
+    z[N]=z[0];
+
+    x[-1]=x[N-1];
+    y[-1]=y[N-1];
+    z[-1]=z[N-1];
+
+
+    for(sweep=0;sweep<meas;sweep++)
+    {
+        for(i=0;i<N;i++)
+        {
+            dx=delta*2*(((double)rand()/(double)RAND_MAX)-.5);
+            dy=delta*2*(((double)rand()/(double)RAND_MAX)-.5);
+            dz=delta*2*(((double)rand()/(double)RAND_MAX)-.5);
+
+            x1=x[i]+dx;
+            y1=y[i]+dy;
+            z1=z[i]+dz;
+
+            //ds= dx *(2*u[i] + dx + g*(u[i-1] + u[i+1]));
+
+
+            ds= (dx*(dx*(2*m + c) + 2*c*x[i] - 2*m*(x[i-1] - 2*x[i] + x[i+1])))/(2*a);
+            ds+= (dy*(dy*(2*m + c) + 2*c*y[i] - 2*m*(y[i-1] - 2*y[i] + y[i+1])))/(2*a);
+            ds+=(dz*(dz*(2*m + c) + 2*c*z[i] - 2*m*(z[i-1] - 2*z[i] + z[i+1])))/(2*a);
+
+
+            //ds= (m/(2*a*a))* (2*dx*dx + 2*dy*dy + 2*dz*dz - 2*dx*x[i-1] + 4*dx*x[i] - 2*dx*x[i+1] - 2 dy y2 + 4 dy y3 - 2 dy y4 - 2 dz z2 + 4 dz z3 + a^2 w^2 Sqrt[x3^2 + y3^2 + z3^2] - a^2 w^2 Sqrt[(dx + x3)^2 + (dy + y3)^2 + (dz + z3)^2] - 2 dz z4)
+
+            //printf("ds= %e \n",exp(-ds));
+
+            if(ds<0){
+                x[i]=x1;
+                y[i]=y1;
+                z[i]=z1;
+                acc+=1;
+            }
+            else{
+                if( ((double)rand()/(double)RAND_MAX) <= exp(-ds) )
+                {
+                    x[i]=x1;
+                    y[i]=y1;
+                    z[i]=z1;
+                    acc+=1;
+                }
+            }
+
+            l+=1;
+            r=acc/l;
+            x[N]=x[0];
+            y[N]=y[0];
+            z[N]=z[0];
+
+            x[-1]=x[N-1];
+            y[-1]=y[N-1];
+            z[-1]=z[N-1];
+        }
+
+        if(sweep == burn + u*ind){
+            x2=0.0;
+            pos1=0.0;
+            pos2=0.0;
+            e=0;e2=0;
+            for(j=0;j<N;j++){
+                x2=x[j]*x[j];
+                pos2+=pow(x[j],2.0);
+                pos2+=pow(y[j],2.0);
+                pos2+=pow(z[j],2.0);
+                pos1+=x[j];
+                pos1+=y[j];
+                pos1+=z[j];
+                fprintf(poshist,"%f  %f  %f \n",x[j], y[j], z[j]);
+                //fprintf(pos2hist,"%f \n",u2);
+                e+=m*w2*x2;
+                e2+=pow(m*w2*x2,2.0);
+            }
+            pos1=pos1/NN;
+            pos2=pos2/NN;
+            ee+=m*pow(w,2.0)*pos2;
+            ee2+=pow(m*pow(w,2.0)*pos2,2.0);
+            //fprintf(poshist,"%f \n",pos1);
+            fprintf(pos2hist,"%f \n",pos2);
+            u+=1;
+            printf("%i   acceptance ratio = %f\n",u,r );
+        }
+
+    }
+    ee=ee/u;
+    ee2=ee2/u;
+    double er=sqrt((ee2-pow(ee,2.0)))/sqrt(sqrt(u));
+    printf("the ground state energy is %f \nthe error in the energy is %f ",ee,er);
+
+}
